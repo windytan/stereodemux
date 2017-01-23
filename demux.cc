a78bd95351f1f74d7989443528843f21830e7eaf
Oona Räisänen <oona@kapsi.fi>
2017-01-23 11:20:55 +0200

    var naming

--- demux.cc
+++ demux.cc
@@ -18,7 +18,7 @@ const float kAudioFIRCutoffHz   = 150000.0f;
 const int   kDeEmphasisOrder    = 2;
 const float kDeEmphasisCutoffHz = 5000.0f;
 
-struct stereosample {
+struct StereoSample {
   int16_t l;
   int16_t r;
 };
@@ -46,35 +46,36 @@ int main(int argc, char **argv) {
   }
 
   int16_t inbuf[kBuflen];
-  stereosample outbuf[kBuflen];
+  StereoSample outbuf[kBuflen];
 
   liquid::NCO nco_pilot_approx(kPilotHz * 2 * M_PI / srate);
   liquid::NCO nco_pilot_exact(kPilotHz * 2 * M_PI / srate);
   nco_pilot_exact.setPLLBandwidth(kPLLBandwidthHz / srate);
-  liquid::NCO nco_stereo(2 * kPilotHz * 2 * M_PI / srate);
+  liquid::NCO nco_stereo_subcarrier(2 * kPilotHz * 2 * M_PI / srate);
   liquid::FIRFilter fir_pilot(1350.0f / srate, kPilotFIRHalfbandHz / srate);
 
-  liquid::WDelay delay(fir_pilot.getGroupDelayAt(4000.0f / srate));
+  liquid::WDelay audio_delay(fir_pilot.getGroupDelayAt(4000.0f / srate));
 
   liquid::FIRFilter fir_l_plus_r(1350.0f / srate, kAudioFIRCutoffHz / srate);
   liquid::FIRFilter fir_l_minus_r(1350.0f / srate, kAudioFIRCutoffHz / srate);
 
-  int N = kDeEmphasisOrder;
-  unsigned int r = N % 2;     // odd/even order
-  unsigned int L = (N-r)/2;   // filter semi-length
+  unsigned int r = kDeEmphasisOrder % 2;     // odd/even order
+  unsigned int L = (kDeEmphasisOrder-r)/2;   // filter semi-length
 
   // filter coefficients arrays
-  float B[3*(L+r)];
-  float A[3*(L+r)];
+  float deemph_coeff_B[3*(L+r)];
+  float deemph_coeff_A[3*(L+r)];
 
   liquid_iirdes(LIQUID_IIRDES_BUTTER, LIQUID_IIRDES_LOWPASS, LIQUID_IIRDES_SOS,
-      N, kDeEmphasisCutoffHz / srate, 0.0f, 10.0f, 10.0f, B, A);
-  iirfilt_crcf iir_deemph_l = iirfilt_crcf_create_sos(B, A, L+r);
-  iirfilt_crcf iir_deemph_r = iirfilt_crcf_create_sos(B, A, L+r);
+      kDeEmphasisOrder, kDeEmphasisCutoffHz / srate, 0.0f, 10.0f, 10.0f,
+      deemph_coeff_B, deemph_coeff_A);
+  iirfilt_crcf iir_deemph_l = iirfilt_crcf_create_sos(deemph_coeff_B,
+      deemph_coeff_A, L+r);
+  iirfilt_crcf iir_deemph_r = iirfilt_crcf_create_sos(deemph_coeff_B,
+      deemph_coeff_A, L+r);
 
   int16_t dc_cancel_buffer[kBuflen] = {0};
   int dc_cancel_sum = 0;
-  float dph = 0.0f;
 
   while (fread(&inbuf, sizeof(inbuf[0]), kBuflen, stdin)) {
     for (int n = 0; n < kBuflen; n++) {
@@ -88,25 +89,23 @@ int main(int argc, char **argv) {
 
       std::complex<float> pilot_bb =
         nco_pilot_approx.mixDown(insample);
-      delay.push(insample);
+      audio_delay.push(insample);
 
       fir_pilot.push(pilot_bb);
 
       std::complex<float> pilot_bp =
         nco_pilot_approx.mixUp(fir_pilot.execute());
 
-      dph = std::arg(pilot_bp * std::conj(nco_pilot_exact.getComplex()));
+      float dph = std::arg(pilot_bp * std::conj(nco_pilot_exact.getComplex()));
 
-      nco_stereo.setPhase(2 * nco_pilot_exact.getPhase());
-
-      std::complex<float> stereo = nco_stereo.mixDown(delay.read());
+      nco_stereo_subcarrier.setPhase(2 * nco_pilot_exact.getPhase());
 
       nco_pilot_approx.step();
       nco_pilot_exact.stepPLL(dph);
       nco_pilot_exact.step();
 
-      fir_l_plus_r.push(delay.read());
-      fir_l_minus_r.push(stereo);
+      fir_l_plus_r.push(audio_delay.read());
+      fir_l_minus_r.push(nco_stereo_subcarrier.mixDown(audio_delay.read()));
       float l_plus_r  = fir_l_plus_r.execute().real();
       float l_minus_r = fir_l_minus_r.execute().real();
 
